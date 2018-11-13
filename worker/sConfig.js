7dcecbd4a2090bdf6632090bc3417971d523c4ce
cfsepulveda <42418118+cfsepulveda@users.noreply.github.com>
2018-11-13 13:47:31 -0500

    Update sConfig.js

--- worker/sConfig.js
+++ worker/sConfig.js
@@ -7,7 +7,7 @@ var sesTransport = require('nodemailer-ses-transport');
 var SparkPost = require('sparkpost');
 var sparky = new SparkPost();
 
-var path_nas = '/tmp/videos/';
+var path_nas = '/tmp/';
 
 var SESCREDENTIALS = {
     accessKeyId: process.env.KEYID || '',
